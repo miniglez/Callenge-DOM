a5541e9175d2a93149c4c0a5f0ba36ec06c05340
Ivan <miniglez.98@gmail.com>
2024-06-12 21:34:24 +0200

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+
+ const button = document.getElementById("agregar");
+
+
+ button.addEventListener("click", function(){
+    const lista = document.getElementById("lista")
+    const element = prompt("Añade un elemento a la lista");
+    lista.innerHTML += "<li>" + element + "</li>";
+ })
+
+ function añadirElementoArray(){
+    
+ }
